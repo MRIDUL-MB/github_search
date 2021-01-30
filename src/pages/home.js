877cf1ebc3286a1de7ea863a3a3e074809e54c9c
MRIDUL-MB <mriravbagla@gmail.com>
2021-01-30 17:36:43 +0530

    made a build folder

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Result from '../components/result';
 import Search from '../components/search';
+import { useGlobalContext } from './../context';
 
 function Home() {
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return <div className='loader'></div>;
+  }
   return (
     <main className='home'>
       <Search />
